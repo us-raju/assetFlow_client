06817554e04d3fddc4b640c2ef11624e87813ee7
us-raju <usraju900@gmail.com>
2025-12-07 12:06:12 +0600

    banner section framer motion effect added

--- src/RootLayout/Home/Banner/Banner.jsx
+++ src/RootLayout/Home/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import bannerImg from "../../../assets/heroImgresize.jpg";
+import { easeInOut, motion } from "framer-motion";
 const Banner = () => {
   return (
     <>
@@ -8,18 +9,31 @@ const Banner = () => {
         style={{ backgroundImage: `url(${bannerImg})` }}
       >
         <div className="max-w-[140px]  lg:max-w-[640px] ">
-          <h1 className="text-[18px] lg:text-5xl text-primary font-semibold mb-3">
+          <motion.h1
+            className="text-[18px] lg:text-5xl text-primary font-semibold mb-3"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
             Simplify HR & Asset Management in One Platform
-          </h1>
-          <p className="text-[12px] lg:text-2xl text-base-content font-medium">
+          </motion.h1>
+
+          <motion.p
+            className="text-[12px] lg:text-2xl text-base-content font-medium"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
             Manage employees, track company assets, and automate HR
             workflows—all from a clean and smart dashboard.
-          </p>
+          </motion.p>
+
           <div className="mt-5 flex flex-col lg:flex-row">
-            <button className="border border-secondary mb-3 lg:mb-0 lg:mr-5 py-1 px-2 lg:py-2 lg:px-4  rounded-[10px] text-[12px] lg:text-[18px] font-medium hover:bg-primary hover:text-base-200">
+            <button className="border border-secondary mb-3 lg:mb-0 lg:mr-5 py-1 px-2 lg:py-2 lg:px-4  rounded-[10px] text-[12px] lg:text-[18px] font-medium hover:bg-primary hover:text-base-200 cursor-pointer">
               Get Started
             </button>
-            <button className="border border-secondary py-1 px-2 lg:py-2 lg:px-4 rounded-[10px] text-[12px] lg:text-[18px] font-medium hover:bg-primary hover:text-base-200">
+       
+            <button className="border border-secondary py-1 px-2 lg:py-2 lg:px-4 rounded-[10px] text-[12px] lg:text-[18px] font-medium hover:bg-primary hover:text-base-200 cursor-pointer">
               View Pricing
             </button>
           </div>
